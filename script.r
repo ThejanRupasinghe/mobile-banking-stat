fee87d8a690b966ab1a2667457907c685038b05b
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2020-12-12 07:56:18 +0530

    Adds files

---
+++ script.r
@@ -0,0 +1,15 @@
+pchisq(8.810866, 3, lower.tail = FALSE) # 0.03191414
+
+pnorm(3.3259, mean = 0, sd = 1, lower.tail = FALSE) # 0.0004406677
+
+pchisq(0.310973, 1, lower.tail = FALSE) # 0.5770837
+
+# T DISTRIBUTION
+# probability for t value
+pt(2, df=49, lower.tail = TRUE)
+# value for probability
+qt(0.9744704, df=49)
+#chi square right area (p value)
+pchisq(9.7, 2, lower.tail = FALSE)
+# value for probability
+qchisq(0.007828378, 2, lower.tail = FALSE)
